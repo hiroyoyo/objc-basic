310091b759ac675d2222f89e21b477e4ad63e4c8
hiroyoyo <k014c1139@it-neec.jp>
2016-12-31 01:38:45 +0900

    Fixed. #7 の課題が完了しました。

---
+++ ex2-1-2/ex2-1-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-1-2
+//
+//  Created by 佐野浩代 on 2016/12/30.
+//  Copyright © 2016年 佐野浩代. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
