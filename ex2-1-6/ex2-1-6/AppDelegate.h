a115e2768f1c2a7d677af622660b6307969bf459
hiroyoyo <k014c1139@it-neec.jp>
2017-01-11 15:08:39 +0900

    Fixed. #11 の課題が完了しました。

---
+++ ex2-1-6/ex2-1-6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex2-1-6
+//
+//  Created by 佐野浩代 on 2017/01/11.
+//  Copyright © 2017年 佐野浩代. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
