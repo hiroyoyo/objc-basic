9e27a73932728a71fed28ea57df139369720ba93
hiroyoyo <k014c1139@it-neec.jp>
2017-01-13 17:00:35 +0900

    Fixed. #13 の課題が完了しました。

---
+++ ex2-1-8/ex2-1-8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ex2-1-8
+//
+//  Created by 佐野浩代 on 2017/01/13.
+//  Copyright © 2017年 佐野浩代. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
